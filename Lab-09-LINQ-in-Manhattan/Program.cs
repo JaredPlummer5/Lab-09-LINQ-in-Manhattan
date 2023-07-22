9c86d9adf2bf4f66d9c19acfd9b340523b1d1322
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-07-21 20:49:22 -0400

    README and Refactor

--- Lab-09-LINQ-in-Manhattan/Program.cs
+++ Lab-09-LINQ-in-Manhattan/Program.cs
@@ -10,6 +10,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        int play = 0;
         string json = File.ReadAllText("../../../data.json"); // replace "data.json" with your file path
 
         ManhatanNeiborhoods? deserializedData = JsonConvert.DeserializeObject<ManhatanNeiborhoods>(json);
@@ -17,56 +18,60 @@ class Program
         if (deserializedData != null && deserializedData.features != null)
         {
             var neighborhoods = (from neighborhoodInfo in deserializedData.features
-                            select new Feature
-                            {
-                                type = neighborhoodInfo.type,
-                                geometry = new Geometry
-                                {
-                                    coordinates = neighborhoodInfo.geometry.coordinates
-                                },
-                                properties = new Properties
-                                {
-                                    zip = neighborhoodInfo.properties.zip,
-                                    city = neighborhoodInfo.properties.city,
-                                    state = neighborhoodInfo.properties.state,
-                                    address = neighborhoodInfo.properties.address,
-                                    borough = neighborhoodInfo.properties.borough,
-                                    neighborhood = neighborhoodInfo.properties.neighborhood,
-                                    county = neighborhoodInfo.properties.county
-                                }
-                            }).ToList();
+                                 select new Feature
+                                 {
+                                     type = neighborhoodInfo.type,
+                                     geometry = new Geometry
+                                     {
+                                         coordinates = neighborhoodInfo.geometry.coordinates
+                                     },
+                                     properties = new Properties
+                                     {
+                                         zip = neighborhoodInfo.properties.zip,
+                                         city = neighborhoodInfo.properties.city,
+                                         state = neighborhoodInfo.properties.state,
+                                         address = neighborhoodInfo.properties.address,
+                                         borough = neighborhoodInfo.properties.borough,
+                                         neighborhood = neighborhoodInfo.properties.neighborhood,
+                                         county = neighborhoodInfo.properties.county
+                                     }
+                                 }).ToList();
 
-
-            Console.WriteLine("Enter 1 to see all of the neighborhoods in Manhatan");
-            Console.WriteLine("Enter 2 to filter all of the neighborhoods with the same name in Manhatan");
-            Console.WriteLine("Enter 3 to remove all of the neighborhoods that are in the same neighborhood in Manhatan");
-
-
-            string action = Console.ReadLine();
-
-
-            if (neighborhoods != null)
+            do
             {
-                switch (action)
+
+                Console.WriteLine("Enter 1 to see all of the neighborhoods in Manhatan");
+                Console.WriteLine("Enter 2 to filter all of the neighborhoods with the same name in Manhatan");
+                Console.WriteLine("Enter 3 to remove all of the neighborhoods that are in the same neighborhood in Manhatan");
+
+
+                string action = Console.ReadLine();
+
+
+                if (neighborhoods != null)
                 {
+                    switch (action)
+                    {
 
-                    case "1":
-                        AllNeighborhoods(neighborhoods);
-                        break;
-                    case "2":
-                        Filter_Out_All_The_Neighborhoods_With_No_Name(neighborhoods);
-                        break;
+                        case "1":
+                            AllNeighborhoods(neighborhoods);
+                            break;
+                        case "2":
+                            Filter_Out_All_The_Neighborhoods_With_No_Name(neighborhoods);
+                            break;
 
-                    case "3":
-                        RewriteAndConsolidate(neighborhoods);
-                        break;
-                    case "4":
-                        Using_Opposing_LINQ_Method(neighborhoods);
-                        break;
+                        case "3":
+                            RewriteAndConsolidate(neighborhoods);
+                            break;
+                        case "4":
+                            Using_Opposing_LINQ_Method(neighborhoods);
+                            break;
 
+                    }
                 }
-            }
 
+            }
+            while (play == 0);
         }
         else
         {
@@ -145,39 +150,10 @@ class Program
     public static void RewriteAndConsolidate(List<Feature> query)
     {
         var rewriteAndConsolidatedQuery = (from neighborhoodInfo in query
-                           where !string.IsNullOrWhiteSpace(neighborhoodInfo.properties.neighborhood)
-                           group neighborhoodInfo by neighborhoodInfo.properties.neighborhood into noDups
+                                           where !string.IsNullOrWhiteSpace(neighborhoodInfo.properties.neighborhood)
+                                           group neighborhoodInfo by neighborhoodInfo.properties.neighborhood into noDups
 
-                           select new Feature
-                           {
-                               type = noDups.First().type,
-                               geometry = new Geometry
-                               {
-                                   coordinates = noDups.First().geometry.coordinates
-                               },
-                               properties = new Properties
-                               {
-                                   zip = noDups.First().properties.zip,
-                                   city = noDups.First().properties.city,
-                                   state = noDups.First().properties.state,
-                                   address = noDups.First().properties.address,
-                                   borough = noDups.First().properties.borough,
-                                   neighborhood = noDups.First().properties.neighborhood,
-                                   county = noDups.First().properties.county
-                               }
-                           }).ToList();
-        AllNeighborhoods(rewriteAndConsolidatedQuery);
-    }
-
-
-
-    public static void Using_Opposing_LINQ_Method(List<Feature> query)
-    {
-
-        var Opposing_Methods = query.Where(neighborhoodInfo => !string.IsNullOrWhiteSpace(neighborhoodInfo.properties.neighborhood)).GroupBy(
-                                           neighborhoodInfo => neighborhoodInfo.properties.neighborhood).Select( noDups =>
-
-                                            new Feature
+                                           select new Feature
                                            {
                                                type = noDups.First().type,
                                                geometry = new Geometry
@@ -195,6 +171,35 @@ class Program
                                                    county = noDups.First().properties.county
                                                }
                                            }).ToList();
+        AllNeighborhoods(rewriteAndConsolidatedQuery);
+    }
+
+
+
+    public static void Using_Opposing_LINQ_Method(List<Feature> query)
+    {
+
+        var Opposing_Methods = query.Where(neighborhoodInfo => !string.IsNullOrWhiteSpace(neighborhoodInfo.properties.neighborhood)).GroupBy(
+                                           neighborhoodInfo => neighborhoodInfo.properties.neighborhood).Select(noDups =>
+
+                                            new Feature
+                                            {
+                                                type = noDups.First().type,
+                                                geometry = new Geometry
+                                                {
+                                                    coordinates = noDups.First().geometry.coordinates
+                                                },
+                                                properties = new Properties
+                                                {
+                                                    zip = noDups.First().properties.zip,
+                                                    city = noDups.First().properties.city,
+                                                    state = noDups.First().properties.state,
+                                                    address = noDups.First().properties.address,
+                                                    borough = noDups.First().properties.borough,
+                                                    neighborhood = noDups.First().properties.neighborhood,
+                                                    county = noDups.First().properties.county
+                                                }
+                                            }).ToList();
 
         Console.WriteLine($"Using opposing methods count: {Opposing_Methods.Count}");
 
